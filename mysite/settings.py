59e4e2ac479794a93b2c55350395e70e97400009
Peter Kuria <petermbaik@gmail.com>
2021-04-27 13:54:52 +0300

    added emailing system

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'petermbaik@gmail.com'
+EMAIL_HOST_PASSWORD = '30269490'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
