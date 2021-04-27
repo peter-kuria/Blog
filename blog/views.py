8f0dfd543d674ec3c4de3ecdfcdf8758c3cd51b4
Peter Kuria <petermbaik@gmail.com>
2021-04-27 21:37:39 +0300

    added tagging functionality

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm 
 from django.core.mail import send_mail
 # Create your views here.
 class PostListView(ListView):
@@ -17,7 +17,25 @@ def post_detail(request, year, month, day, post):
     status='published', publish__year=year,
     publish__month=month, publish__day=day)
 
-    return render(request, 'blog/post/detail.html',{'post':post})
+    #list of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #create a comment object but dont save to thedatabase yet
+            new_comment= comment_form.save(commit=False)
+            #assign the current bpost to the comment.post
+            new_comment.post=post
+
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()        
+
+    return render(request, 'blog/post/detail.html',{'post':post, 'comments': comments, 'new_comment': new_comment,'comment_form': comment_form})
 
 def post_share(request, post_id):
      #retreive post by id
@@ -35,7 +53,8 @@ def post_share(request, post_id):
 
              send_mail(subject,message, 'petermbaik@gmail.com', [cd['to']]) 
 
-             sent = True            
+             sent = True 
+
              
 
      else:
