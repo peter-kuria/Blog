3538d961bdc91cd472197aeba14a31591e897c78
Peter Kuria <petermbaik@gmail.com>
2021-04-28 21:33:09 +0300

    updated tagging object

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
+from django.db.models import Count
 # Create your models here.
 
 class PublishedManager(models.Manager):
